482f3fabac8969adc10d1ea383cc21885bf93cf0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-20 06:53:22 +0545

    defined routes and layout wrapper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-red-500">hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout>hello</Layout>} />
+        <Route path="/home" element={<Layout>Home</Layout>} />
+        <Route path="/about" element={<Layout>About</Layout>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
