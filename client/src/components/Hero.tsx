c0d8745d0b7c16aa4da9585e4eb23395514c4f62
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-20 06:52:43 +0545

    hero comp

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className=" bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-5xl text-white font-bold tracking-tight">
+          Find your next trip
+        </h1>
+        <p className="text-2xl text-white">
+          search for your next trip with ease
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
