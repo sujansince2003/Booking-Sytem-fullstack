67754ab29e8a0f912a24ec9d44c16a7b9a3ca273
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-20 08:33:07 +0545

    added the route for handling user registration

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,9 @@ import express, { Request, Response } from "express"
 import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
+import userRoutes from "./routes/users"
 const app = express()
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,9 +14,12 @@ mongoose.connect(process.env.MONGODB_URI as string).then(() => {
     console.log("connected")
 })
 
+
 app.get("/", (req: Request, res: Response) => {
     res.json({ msg: "hello" })
 })
+
+app.use("/api/user", userRoutes)
 
 app.listen(8000, () => {
     console.log("BACKEND SERVER::running in port  8000. visit: http://localhost:8000")
