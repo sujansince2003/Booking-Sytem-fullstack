b6604193a27f7c6cbb466c1c4b048ade63621fb9
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-23 00:01:11 +0545

    built api client to handle request to register backend route

---
+++ client/src/apiClient/register-api-client.ts
@@ -0,0 +1,21 @@
+import type { RegisterFormData } from "../pages/Register";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export const registerAPI = async (formData: RegisterFormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/user/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+
+    })
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+        throw new Error(responseData.msg)
+    }
+    return responseData
+}
