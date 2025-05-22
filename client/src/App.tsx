a0da00620e37222bd81402a1f0acce1e4cdcc15e
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-22 23:59:29 +0545

    defined route for register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import Layout from "./layout/Layout";
+import { Register } from "./pages";
 
 function App() {
   return (
@@ -14,6 +15,14 @@ function App() {
         <Route path="/" element={<Layout>hello</Layout>} />
         <Route path="/home" element={<Layout>Home</Layout>} />
         <Route path="/about" element={<Layout>About</Layout>} />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
