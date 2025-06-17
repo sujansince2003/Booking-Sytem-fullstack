21085d4aa698ba7e661d15c418e4679b3ed300f0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-06-17 18:22:53 +0545

    built the appcontext provider component to handle the state of the application in the client side

--- client/src/contexts/AppContextProvider.tsx
+++ client/src/contexts/AppContextProvider.tsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import { AppContext } from "./AppContext";
+import type { toastMsgType } from "./AppContext";
+import Toast from "../components/Toast";
+import { useQuery } from "@tanstack/react-query";
+import { validateTokenAPIClient } from "../apiClient/validateToken-api-client";
 
 export const AppContextProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  const [toast, setToast] = useState<toastMsgType | undefined>(undefined);
+  const { isError } = useQuery({
+    queryKey: ["validateToken"],
+    queryFn: () => validateTokenAPIClient(),
+    retry: false,
+  });
   return (
     <AppContext.Provider
       value={{
         showToast: (toastMsg) => {
-          console.log(toastMsg);
+          setToast(toastMsg);
         },
+        isLoggedIn: !isError,
       }}
     >
+      {toast && (
+        <Toast
+          toastmsg={toast.toastmsg}
+          type={toast.type}
+          onClose={() => setToast(undefined)}
+        />
+      )}
       {children}
     </AppContext.Provider>
   );
