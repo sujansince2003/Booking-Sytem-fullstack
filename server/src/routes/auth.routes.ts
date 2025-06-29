b26acea811daf6d85c4a5ab844c82d179d63d813
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-06-29 16:12:39 +0545

    build: logout API route handler to handle logout in server

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -72,4 +72,12 @@ router.get("/validate-token", verifyToken, async (req: Request, res: Response) =
     return;
 })
 
+
+router.post("/logout", (req: Request, res: Response) => {
+    res.cookie("auth_token", "", {
+        expires: new Date(0)
+    })
+    res.status(200).send("");
+    return;
+})
 export default router;
